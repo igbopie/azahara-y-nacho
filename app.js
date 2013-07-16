d139214edfd1cd5050a85e69d2d4a2f4f317aa85
Ignacio Bona <igbopie@gmail.com>
2013-07-16 21:48:46 +0200

    socket.io por heroku

--- app.js
+++ app.js
@@ -46,13 +46,6 @@ server.listen(app.get('port'), function(){
 //--------------------
 //Web sockets app
 //--------------------
-
-// Port where we'll run the websocket server
-var webSocketsServerPort = 1337;
-
-// websocket and http servers
-var webSocketServer = require('websocket').server;
-
 /**
  * Global variables
  */
@@ -60,7 +53,6 @@ var webSocketServer = require('websocket').server;
 var history = [ ];
 // list of currently connected clients (users)
 var clients = [ ];
-
 /**
  * Helper function for escaping input strings
  */
@@ -74,26 +66,20 @@ var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
 // ... in random order
 colors.sort(function(a,b) { return Math.random() > 0.5; } );
 
-/**
- * WebSocket server
- */
-var wsServer = new webSocketServer({
-    // WebSocket server is tied to a HTTP server. WebSocket request is just
-    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
-    httpServer: server
+
+
+var io = require('socket.io').listen(server);
+//HEROKU
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
 });
 
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
-wsServer.on('request', function(request) {
-    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+io.on('connection', function (socket) {
+    console.log((new Date()) + ' Connection from origin ' + socket + '.');
 
-    // accept connection - you should check 'request.origin' to make sure that
-    // client is connecting from your website
-    // (http://en.wikipedia.org/wiki/Same_origin_policy)
-    var connection = request.accept(null, request.origin); 
-    // we need to know client index to remove them on 'close' event
-    var index = clients.push(connection) - 1;
+   
+    var index = clients.push(socket) - 1;
     var userName = false;
     var userColor = false;
 
@@ -101,46 +87,42 @@ wsServer.on('request', function(request) {
 
     // send back chat history
     if (history.length > 0) {
-        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+    	socket.emit('history',history);
     }
+	  // user sent some message
+    socket.on('setname', function(message) {
+     	// remember user name
+            userName = htmlEntities(message);
+            // get random color and send it back to the user
+            userColor = colors.shift();
+            
+            socket.emit('color',userColor);
+            console.log((new Date()) + ' User is known as: ' + userName
+                        + ' with ' + userColor + ' color.');
 
-    // user sent some message
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') { // accept only text
-            if (userName === false) { // first message sent by user is their name
-                // remember user name
-                userName = htmlEntities(message.utf8Data);
-                // get random color and send it back to the user
-                userColor = colors.shift();
-                connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
-                console.log((new Date()) + ' User is known as: ' + userName
-                            + ' with ' + userColor + ' color.');
-
-            } else { // log and broadcast the message
-                console.log((new Date()) + ' Received Message from '
-                            + userName + ': ' + message.utf8Data);
-                
-                // we want to keep history of all sent messages
-                var obj = {
-                    time: (new Date()).getTime(),
-                    text: htmlEntities(message.utf8Data),
-                    author: userName,
-                    color: userColor
-                };
-                history.push(obj);
-                history = history.slice(-100);
-
-                // broadcast message to all connected clients
-                var json = JSON.stringify({ type:'message', data: obj });
-                for (var i=0; i < clients.length; i++) {
-                    clients[i].sendUTF(json);
-                }
-            }
-        }
     });
+    // user sent some message
+    socket.on('message', function(message) {
+        console.log((new Date()) + ' Received Message from '
+                    + userName + ': ' + message);
+        
+        // we want to keep history of all sent messages
+        var obj = {
+            time: (new Date()).getTime(),
+            text: htmlEntities(message),
+            author: userName,
+            color: userColor
+        };
+        history.push(obj);
+        history = history.slice(-100);
+
+        for (var i=0; i < clients.length; i++) {
+            clients[i].emit('message',obj);
+        }
+	});
 
     // user disconnected
-    connection.on('close', function(connection) {
+    socket.on('close', function(connection) {
         if (userName !== false && userColor !== false) {
             console.log((new Date()) + " Peer "
                 + connection.remoteAddress + " disconnected.");
@@ -152,3 +134,8 @@ wsServer.on('request', function(request) {
     });
 
 });
+
+
+
+
+
--- public/javascripts/websockets.js
+++ public/javascripts/websockets.js
@@ -11,66 +11,43 @@ $(function () {
     // my name sent to the server
     var myName = false;
 
-    // if user is running mozilla then use it's built-in WebSocket
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-
-    // if browser doesn't support WebSocket, just show some notification and exit
-    if (!window.WebSocket) {
-        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
-                                    + 'support WebSockets.'} ));
-        input.hide();
-        $('span').hide();
-        return;
-    }
-    
-    // open connection
-    var connection = new WebSocket('ws://'+window.location.host);
-
-    connection.onopen = function () {
+   
+   var sio = io.connect('http://'+window.location.host);
+   sio.socket.on('error', function (reason){
+   		// just in there were some problems with conenction...
+   		content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+                                    + 'connection or the server is down.' } ));
+   });
+   sio.on('connect', function (){
+   
         // first we want users to enter their names
         input.removeAttr('disabled');
-        status.text('Choose name:');
-    };
-
-    connection.onerror = function (error) {
-        // just in there were some problems with conenction...
-        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
-                                    + 'connection or the server is down.' } ));
-    };
-
-    // most important part - incoming messages
-    connection.onmessage = function (message) {
-        // try to parse JSON message. Because we know that the server always returns
-        // JSON this should work without any problem but we should make sure that
-        // the massage is not chunked or otherwise damaged.
-        try {
-            var json = JSON.parse(message.data);
-        } catch (e) {
-            console.log('This doesn\'t look like a valid JSON: ', message.data);
-            return;
-        }
-
-        // NOTE: if you're not sure about the JSON structure
-        // check the server source code above
-        if (json.type === 'color') { // first response from the server with user's color
-            myColor = json.data;
-            status.text(myName + ': ').css('color', myColor);
-            input.removeAttr('disabled').focus();
-            // from now user can start sending messages
-        } else if (json.type === 'history') { // entire message history
-            // insert every single message to the chat window
-            for (var i=0; i < json.data.length; i++) {
-                addMessage(json.data[i].author, json.data[i].text,
-                           json.data[i].color, new Date(json.data[i].time));
-            }
-        } else if (json.type === 'message') { // it's a single message
-            input.removeAttr('disabled'); // let the user write another message
-            addMessage(json.data.author, json.data.text,
-                       json.data.color, new Date(json.data.time));
-        } else {
-            console.log('Hmm..., I\'ve never seen JSON like this: ', json);
-        }
-    };
+		status.text('Choose name:');
+   
+   });
+    
+    sio.on('message',function (message) {
+       input.removeAttr('disabled'); // let the user write another message
+       addMessage(message.author, message.text,
+                   message.color, new Date(message.time));
+       
+    });
+    
+    sio.on('color',function (message) {
+       myColor = message;
+       status.text(myName + ': ').css('color', myColor);
+       input.removeAttr('disabled').focus();
+       // from now user can start sending messages
+    });
+    
+    sio.on('history',function (message) {
+       // insert every single message to the chat window
+       for (var i=0; i < message.length; i++) {
+            addMessage(message[i].author,message[i].text,
+                       message[i].color, new Date(message[i].time));
+       }
+        
+    });
 
     /**
      * Send mesage when user presses Enter key
@@ -81,32 +58,25 @@ $(function () {
             if (!msg) {
                 return;
             }
-            // send the message as an ordinary text
-            connection.send(msg);
-            $(this).val('');
-            // disable the input field to make the user wait until server
-            // sends back response
-            input.attr('disabled', 'disabled');
-
+            
             // we know that the first message sent from a user their name
             if (myName === false) {
                 myName = msg;
+                sio.emit("setname",myName);
+                // disable the input field to make the user wait until server
+				// sends back response
+            	input.attr('disabled', 'disabled');
+            }else{
+	            // send the message as an ordinary text
+				sio.emit("message",msg);
             }
+            
+            $(this).val('');
+            
+
+            
         }
     });
-
-    /**
-     * This method is optional. If the server wasn't able to respond to the
-     * in 3 seconds then show some error message to notify the user that
-     * something is wrong.
-     */
-    setInterval(function() {
-        if (connection.readyState !== 1) {
-            status.text('Error');
-            input.attr('disabled', 'disabled').val('Unable to comminucate '
-                                                 + 'with the WebSocket server.');
-        }
-    }, 3000);
 
     /**
      * Add message to the chat window
